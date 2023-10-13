9d2204563b281146c63e41f62f906b1051077630
Mois24 <artyommois024@gmail.com>
2023-10-13 17:40:37 +0200

    init: start react project marvel, feat: add layout and some requests on server for receiving data

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
+import './style/style.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App/>
+  </StrictMode>
+);
+
