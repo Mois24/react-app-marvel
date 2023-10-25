d3a250f0b9f529afd2671e68b99160cc7b16bb13
Mois24 <artyommois024@gmail.com>
2023-10-25 14:33:37 +0200

    fix: return StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
@@ -6,6 +6,8 @@ import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <StrictMode>
+    <App/>
+  </StrictMode>
 );
 
